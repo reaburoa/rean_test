cec362e2ee471be8057c2210d39a6c110afa061b
zhanglei <zhang.lei1@71dai.com>
2015-08-17 15:12:12 +0800

    framework

--- framework/base/Base.php
+++ framework/base/Base.php
@@ -4,17 +4,8 @@ use Jaf;
 
 class Base extends Jaf\base\Service
 {
-    public function __construct($config = array())
-    {
-        if (!empty($config)) {
-            Jaf::configure($this, $config);
-        }
-        $this->init();
-    }
 
-    public function init()
-    {
 
-    }
+
 
 }
--- framework/base/Service.php
+++ framework/base/Service.php
@@ -6,6 +6,23 @@ class Service
 {
     protected static $_coreComponents = array();
 
+    public $controllerNamespace = '';
+
+    public function __construct($config = array())
+    {
+        if (!empty($config)) {
+            Jaf::configure($this, $config);
+        }
+
+        $this->init();
+    }
+
+    public function init()
+    {
+        $con = 'site';echo $this->controllerNamespace."\\".$con."Controller";exit;
+        var_dump(class_exists($this->controllerNamespace."\\".$con."Controller"));
+    }
+
     public static function set($name, $value)
     {
         return self::$_coreComponents[$name] = $value;
