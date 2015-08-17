94606a91724700c33583d8a762db1287749704f6
zhanglei <zhang.lei1@71dai.com>
2015-08-17 11:37:58 +0800

    framework

--- test/index.php
+++ test/index.php
@@ -1,9 +1,7 @@
 <?php
-    /*$jaf = dirname(__FILE__).'/../framework/jaf.php';
+    $jaf = dirname(__FILE__).'/../framework/jaf.php';
     $config = require_once(dirname(__FILE__).'/protected/config/config.php');
     require_once($jaf);
     $application = new Jaf\base\Application($config);
-    $application->run();*/
-include 'protected/controller/siteController.php';
-var_dump(get_class(new test\controller\siteController()));
-//$site->indexAction();
+    $application->run();
+
