c263e581947bab2c12f5e29899dc3eafaeb9967a
reaburoa <296875751@qq.com>
2015-11-29 18:55:12 +0800

    jaf

--- test/index.php
+++ test/index.php
@@ -1,6 +1,8 @@
 <?php
+# This is jaf test
     $jaf = dirname(__FILE__).'/../framework/jaf.php';
     $config = require_once(dirname(__FILE__).'/protected/config/config.php');
     require_once($jaf);
     $application = new Jaf\web\Application($config);
     $application->run();
+
