6823b17619b8c7d4f584b6b83e946e67526f7991
reaburoa <296875751@qq.com>
2015-11-29 17:27:13 +0800

    jaf

--- test/protected/controllers/siteController.php
+++ test/protected/controllers/siteController.php
@@ -6,7 +6,6 @@ class siteController extends Controller
 {
     public function indexAction()
     {
-        //var_dump(\Jaf::$app->getRequest());
-        echo '<br />index html!';
+        echo 'index html!';
     }
 }
